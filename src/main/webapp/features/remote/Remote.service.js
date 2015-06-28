54ec7243b005fab3810099e8444af836c2de78c7
Skisor <syrovets@gmail.com>
2015-06-28 14:20:57 +0300

    Front-end: Change request data format to "x-www-form-urlencoded" (Again)

--- src/main/webapp/features/remote/Remote.service.js
+++ src/main/webapp/features/remote/Remote.service.js
@@ -3,16 +3,33 @@
 angular.module('catan')
     .provider('Remote', function () {
 
-        var Remote = {};
-
-        var http;
+        var Remote = {},
+            http,
+            defaultParams = {},
+            groupParams = {};
 
         return {
+            setDefault: function(params) {
+                defaultParams = params;
+            },
+
+            setGroup: function(group, params) {
+                groupParams[group] = params;
+            },
+
             setRequest: function (group, name, params) {
+
+                var finalParams = angular.extend(
+                    {},
+                    defaultParams,
+                    (groupParams[group]) ? groupParams[group] : {},
+                    params
+                );
+
                 Remote[group] = Remote[group] || {};
                 Remote[group][name] = function(data) {
-                    params.data = data;
-                    return http(params);
+                    finalParams.data = data;
+                    return http(finalParams);
                 };
             },
 
