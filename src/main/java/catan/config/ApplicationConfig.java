813109c51277f104f9ddad038493a40206311825
oborkovskyi <oborkovskyi@playtika.com>
2015-07-07 17:24:58 +0300

    removed unused bean

--- src/main/java/catan/config/ApplicationConfig.java
+++ src/main/java/catan/config/ApplicationConfig.java
@@ -3,9 +3,7 @@ package catan.config;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 @ComponentScan({"catan"})
@@ -14,14 +12,5 @@ public class ApplicationConfig extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(ApplicationConfig.class);
-    }
-
-    //Do we need it?
-    @Bean
-    public InternalResourceViewResolver configureInternalResourceViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/");
-        resolver.setSuffix(".jsp");
-        return resolver;
     }
 }
