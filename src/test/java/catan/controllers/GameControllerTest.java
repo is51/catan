d673ca6f2e1b9bb769acc3233f7b062eb578412b
Skisor <syrovets@gmail.com>
2015-07-09 23:32:11 +0300

    [US-7] Add more tests (2 left for TODO)

--- src/test/java/catan/controllers/GameControllerTest.java
+++ src/test/java/catan/controllers/GameControllerTest.java
@@ -2,7 +2,6 @@ package catan.controllers;
 
 import catan.config.ApplicationConfig;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -12,10 +11,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.isEmptyString;
-import static org.hamcrest.Matchers.lessThanOrEqualTo;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.hamcrest.Matchers.isA;
+import static org.hamcrest.Matchers.closeTo;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -23,70 +19,72 @@ import static org.hamcrest.Matchers.isA;
 @WebIntegrationTest("server.port:8091")
 public class GameControllerTest {
     public static final int SERVER_PORT = 8091;
-    public static final String CONTENT_TYPE = "application/json";
+    public static final String ACCEPT_CONTENT_TYPE = "application/json";
 
     public static final String URL_CREATE_NEW_GAME = "/api/game/create";
+    public static final String URL_CURRENT_GAMES_LIST = "/api/game/list/current";
+    public static final String URL_PUBLIC_GAMES_LIST = "/api/game/list/current";
 
     public static final String USER_NAME = "test game";
     public static final String USER_PASSWORD = "test game";
 
-    private String userToken;
+    private static boolean initialized = false;
 
     private void registerUser(String username, String password) {
-        given().
-                port(SERVER_PORT).
-                header("Accept", CONTENT_TYPE).
-                parameters("username", username, "password", password).
-        when().
-                post("/api/user/register").
-        then().
-                statusCode(200);
+        given()
+            .port(SERVER_PORT)
+            .header("Accept", ACCEPT_CONTENT_TYPE)
+            .parameters("username", username, "password", password)
+        .when()
+            .post("/api/user/register")
+        .then()
+            .statusCode(200);
     }
 
     private String loginUser(String username, String password) {
         return
-            given().
-                    port(SERVER_PORT).
-                    header("Accept", CONTENT_TYPE).
-                    parameters("username", username, "password", password).
-                    when().
-                    post("/api/user/login").
-            then().
-                    statusCode(200).
-            extract()
-                    .path("token");
+            given()
+                .port(SERVER_PORT)
+                .header("Accept", ACCEPT_CONTENT_TYPE)
+                .parameters("username", username, "password", password)
+                .when()
+                .post("/api/user/login")
+            .then()
+                .statusCode(200)
+            .extract()
+                .path("token");
     }
 
     private void logoutUser(String token) {
-        given().
-                port(SERVER_PORT).
-                header("Accept", CONTENT_TYPE).
-                parameters("token", token)
-        .when().
-                post("/api/user/logout").
-        then().
-                statusCode(200);
+        given()
+            .port(SERVER_PORT)
+            .header("Accept", ACCEPT_CONTENT_TYPE)
+            .parameters("token", token)
+        .when()
+            .post("/api/user/logout")
+        .then()
+            .statusCode(200);
     }
 
     private int getUserId(String token) {
         return
-            given().
-                    port(SERVER_PORT).
-                    header("Content-Type", CONTENT_TYPE).
-                    parameters("token", token).
-            when().
-                    post("/api/user/details").
-            then().
-                    statusCode(200).
-            extract()
-                    .path("id");
+            given()
+                .port(SERVER_PORT)
+                .header("Accept", ACCEPT_CONTENT_TYPE)
+                .parameters("token", token)
+            .when()
+                .post("/api/user/details")
+            .then()
+                .statusCode(200)
+            .extract()
+                .path("id");
     }
 
     @Before
     public void setup(){
-        if(userToken == null){
+        if(!initialized){
             registerUser(USER_NAME, USER_PASSWORD);
-            userToken = loginUser(USER_NAME, USER_PASSWORD);
+            initialized = true;
         }
     }
 
@@ -95,75 +93,149 @@ public class GameControllerTest {
     // ----------------
 
     @Test
-    public void shouldSuccessfullyCreateNewGame(){
+    public void should_successfully_create_new_game(){
+        String userToken = loginUser(USER_NAME, USER_PASSWORD);
         int userId = getUserId(userToken);
 
-        given().
-                port(SERVER_PORT).
-                header("Content-Type", CONTENT_TYPE).
-                parameters("token", userToken, "privateGame", true).
-        when().
-                post(URL_CREATE_NEW_GAME).
-        then().
-                statusCode(200).
-                contentType(CONTENT_TYPE).
-                body("gameId", greaterThan(0)).
-             and().
-                body("creatorId", equalTo(userId)).
-             and().
-                body("privateGame", equalTo(true)).
-             and().
-                body("dateCreated", lessThanOrEqualTo(System.currentTimeMillis())).
-             and().
-                body("dateCreated", greaterThanOrEqualTo(System.currentTimeMillis() - 1 * 60 * 1000)); // 1 minute
+        given()
+            .port(SERVER_PORT)
+            .header("Accept", ACCEPT_CONTENT_TYPE)
+            .parameters("token", userToken, "privateGame", true)
+        .when()
+            .post(URL_CREATE_NEW_GAME)
+        .then()
+            .statusCode(200)
+            .contentType(ACCEPT_CONTENT_TYPE)
+            .body("gameId", greaterThan(0))
+                .and()
+            .body("creatorId", equalTo(userId))
+                .and()
+            .body("privateGame", equalTo(true))
+                .and()
+            .body("dateCreated", closeTo(System.currentTimeMillis(), 60000)); // +-1 minute
     }
 
     @Test
-    public void shouldFailWithErrorWhenThereIsNoPrivateGameVariable(){
-        given().
-                port(SERVER_PORT).
-                header("Content-Type", CONTENT_TYPE).
-                parameters("token", userToken).
-        when().
-                post(URL_CREATE_NEW_GAME).
-        then().
-                statusCode(400);
+    public void should_fail_with_error_when_there_is_no_private_game_variable(){
+        String userToken = loginUser(USER_NAME, USER_PASSWORD);
+
+        given()
+            .port(SERVER_PORT)
+            .header("Accept", ACCEPT_CONTENT_TYPE)
+            .parameters("token", userToken)
+        .when()
+            .post(URL_CREATE_NEW_GAME)
+        .then()
+            .statusCode(400);
     }
 
     @Test
-    public void shouldFailWith403ErrorWhenUserIsNotAuthorized(){
+    public void should_fail_with_403_error_when_user_is_not_authorized(){
+        String userToken = loginUser(USER_NAME, USER_PASSWORD);
 
         // without token
 
-        given().
-                port(SERVER_PORT).
-                header("Content-Type", CONTENT_TYPE).
-        when().
-                post(URL_CREATE_NEW_GAME).
-        then().
-                statusCode(403);
+        given()
+            .port(SERVER_PORT)
+            .header("Accept", ACCEPT_CONTENT_TYPE)
+        .when()
+            .post(URL_CREATE_NEW_GAME)
+        .then()
+            .statusCode(403);
 
         // with old token
 
         logoutUser(userToken);
 
-        given().
-                port(SERVER_PORT).
-                header("Content-Type", CONTENT_TYPE).
-                parameters("token", userToken).
-        when().
-                post(URL_CREATE_NEW_GAME).
-        then().
-                statusCode(403);
+        given()
+            .port(SERVER_PORT)
+            .header("Accept", ACCEPT_CONTENT_TYPE)
+            .parameters("token", userToken)
+        .when()
+            .post(URL_CREATE_NEW_GAME)
+        .then()
+            .statusCode(403);
     }
 
     // ----------------------
     // List of Current Games
     // ----------------------
+    
+    @Test
+    public void should_get_current_games_list_with_special_parameters(){
+        //String userToken = loginUser(USER_NAME, USER_PASSWORD);
+        //int userId = getUserId(userToken);
 
-    // ..................
-    // ..............
-    // ..........
+        //TODO: check properties of all objects in array (gameId, creatorId, privateGame, dateCreated)
+    }
+
+    @Test
+    public void should_fail_with_403_error_when_getting_current_games_list_if_user_is_not_authorized(){
+
+        String userToken = loginUser(USER_NAME, USER_PASSWORD);
+
+        // without token
+
+        given()
+            .port(SERVER_PORT)
+            .header("Accept", ACCEPT_CONTENT_TYPE)
+        .when()
+            .post(URL_CURRENT_GAMES_LIST)
+        .then()
+            .statusCode(403);
+
+        // with old token
+
+        logoutUser(userToken);
+
+        given()
+            .port(SERVER_PORT)
+            .header("Accept", ACCEPT_CONTENT_TYPE)
+            .parameters("token", userToken)
+        .when()
+            .post(URL_CURRENT_GAMES_LIST)
+        .then()
+            .statusCode(403);
+    }
+
+    // ----------------------
+    // List of Public Games
+    // ----------------------
+
+    @Test
+    public void should_get_public_games_list_with_special_parameters(){
+
+        //TODO: check properties of all objects in array (gameId, creatorId, privateGame, dateCreated)
+    }
+
+    @Test
+    public void should_get_public_games_list_even_if_user_is_not_authorized(){
+
+        String userToken = loginUser(USER_NAME, USER_PASSWORD);
+
+        // without token
+
+        given()
+            .port(SERVER_PORT)
+            .header("Accept", ACCEPT_CONTENT_TYPE)
+        .when()
+            .post(URL_PUBLIC_GAMES_LIST)
+        .then()
+            .statusCode(200);
+
+        // with old token
+
+        logoutUser(userToken);
+
+        given()
+            .port(SERVER_PORT)
+            .header("Accept", ACCEPT_CONTENT_TYPE)
+            .parameters("token", userToken)
+        .when()
+            .post(URL_PUBLIC_GAMES_LIST)
+        .then()
+            .statusCode(200);
+    }
 
 
 }
