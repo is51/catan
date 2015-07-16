5eeed89a2f144607f0d17d157451e06b36da0e1b
oborkovskyi <oborkovskyi@playtika.com>
2015-07-16 18:46:49 +0300

    BG-7: Disabled ErrorMvcAutoConfiguration

--- src/main/java/catan/config/ApplicationConfig.java
+++ src/main/java/catan/config/ApplicationConfig.java
@@ -1,12 +1,15 @@
 package catan.config;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
 @ComponentScan({"catan"})
+@EnableAutoConfiguration(exclude=ErrorMvcAutoConfiguration.class)
 public class ApplicationConfig extends SpringBootServletInitializer {
 
     @Override
