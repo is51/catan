a6d439206c4879e934888b845a64bf245dd8206c
andrey <ryzhenko.andrii@gmail.com>
2015-08-16 14:45:09 +0300

    [US-26] @T136, Add field moveOrder to model

--- src/main/java/catan/domain/model/game/GameUserBean.java
+++ src/main/java/catan/domain/model/game/GameUserBean.java
@@ -27,6 +27,9 @@ public class GameUserBean {
     @Column(name = "READY", nullable = false)
     private boolean ready;
 
+    @Column(name = "MOVE_ORDER", unique = false, nullable = false)
+    private int moveOrder;
+
     public GameUserBean() {
     }
 
@@ -65,6 +68,14 @@ public class GameUserBean {
 
     public void setReady(boolean ready) {
         this.ready = ready;
+    }
+
+    public int getMoveOrder() {
+        return moveOrder;
+    }
+
+    public void setMoveOrder(int moveOrder) {
+        this.moveOrder = moveOrder;
     }
 
     @Override
