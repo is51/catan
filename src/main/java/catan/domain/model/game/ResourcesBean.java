a25209b748cb3bbb37457428c1ad941351014906
ypetrenko <yuriy.a.petrenko@gmail.com>
2015-08-20 13:34:16 +0300

    [US-27] @T148, Add resources fields to domain model

---
+++ src/main/java/catan/domain/model/game/ResourcesBean.java
@@ -0,0 +1,95 @@
+package catan.domain.model.game;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import javax.persistence.*;
+
+import static org.apache.commons.lang.builder.ToStringStyle.SHORT_PREFIX_STYLE;
+
+@Entity
+@Table(name = "RESOURCES")
+public class ResourcesBean {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "RESOURCES_ID", unique = true, nullable = false)
+    private int resourcesId;
+
+    @Column(name = "BRICK", nullable = false)
+    private int brick;
+
+    @Column(name = "WOOD", nullable = false)
+    private int wood;
+
+    @Column(name = "SHEEP", nullable = false)
+    private int sheep;
+
+    @Column(name = "WHEAT", nullable = false)
+    private int wheat;
+
+    @Column(name = "STONE", nullable = false)
+    private int stone;
+
+    public ResourcesBean() {
+    }
+
+    public ResourcesBean(int brick, int wood, int sheep, int wheat, int stone) {
+        this.brick = brick;
+        this.wood = wood;
+        this.sheep = sheep;
+        this.wheat = wheat;
+        this.stone = stone;
+    }
+
+    public int getResourcesId() {
+        return resourcesId;
+    }
+
+    public void setResourcesId(int resourcesId) {
+        this.resourcesId = resourcesId;
+    }
+
+    public int getBrick() {
+        return brick;
+    }
+
+    public void setBrick(int brick) {
+        this.brick = brick;
+    }
+
+    public int getWood() {
+        return wood;
+    }
+
+    public void setWood(int wood) {
+        this.wood = wood;
+    }
+
+    public int getSheep() {
+        return sheep;
+    }
+
+    public void setSheep(int sheep) {
+        this.sheep = sheep;
+    }
+
+    public int getWheat() {
+        return wheat;
+    }
+
+    public void setWheat(int wheat) {
+        this.wheat = wheat;
+    }
+
+    public int getStone() {
+        return stone;
+    }
+
+    public void setStone(int stone) {
+        this.stone = stone;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, SHORT_PREFIX_STYLE);
+    }
+}
