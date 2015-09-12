e5433da296e75216f19a098985cc646284f7bca3
andrey <ryzhenko.andrii@gmail.com>
2015-09-12 15:14:37 +0300

    [US-39] @T190, merge with master

--- src/main/java/catan/controllers/PlayController.java
+++ src/main/java/catan/controllers/PlayController.java
@@ -7,7 +7,10 @@ import catan.domain.model.user.UserBean;
 import catan.services.AuthenticationService;
 import catan.services.PlayService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
