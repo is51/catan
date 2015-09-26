99008865c5bbd2bf331aa57448bc1d76ce3b9e96
ypetrenko <yuriy.a.petrenko@gmail.com>
2015-09-27 00:39:44 +0300

    [US-40] update build road tests

--- src/test/java/catan/controllers/play/BuildRoadTest.java
+++ src/test/java/catan/controllers/play/BuildRoadTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -43,8 +44,157 @@ public class BuildRoadTest extends PlayTestUtil {
         String userToken3 = loginUser(USER_NAME_3, USER_PASSWORD_3);
 
         int gameId = createNewGame(userToken1, false).path("gameId");
-        int edgeId = viewGame(userToken1, gameId).path("map.edges[0].edgeId");
-        int firstUserId = getUserId(userToken1);
+        int nodeIdToBuild = viewGame(userToken1, gameId).path("map.hexes[0].nodes.topLeftId");
+        int edgeIdToBuild = viewGame(userToken1, gameId).path("map.hexes[0].edges.topLeftId");
+        int gameUserId1 = viewGame(userToken1, gameId).path("gameUsers[0].id");
+
+        joinPublicGame(userToken2, gameId);
+        joinPublicGame(userToken3, gameId);
+
+        setUserReady(userToken1, gameId);
+        setUserReady(userToken2, gameId);
+        setUserReady(userToken3, gameId);
+
+        buildSettlement(userToken1, gameId, nodeIdToBuild);
+
+        viewGame(userToken1, gameId)
+                .then()
+                .statusCode(200)
+                .body("map.hexes[0].edges.topLeftId", is(edgeIdToBuild))
+                .body("map.edges.find {it.edgeId = " + edgeIdToBuild + "}.building", nullValue())
+                .body("status", equalTo("PLAYING"));
+
+        buildRoad(userToken1, gameId, edgeIdToBuild);
+
+        viewGame(userToken1, gameId)
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("PLAYING"))
+                .body("map.hexes[0].edges.topLeftId", is(edgeIdToBuild))
+                .rootPath("map.edges.find {it.edgeId == " + edgeIdToBuild + "}")
+                .body("building", notNullValue())
+                .body("building.ownerGameUserId", is(gameUserId1))
+                .body("building.built", equalTo("ROAD"));
+
+    }
+
+    @Test
+    public void should_successfully_build_road_on_empty_edge_if_has_neighbour_road_that_belongs_to_this_player() {
+        String userToken1 = loginUser(USER_NAME_1, USER_PASSWORD_1);
+        String userToken2 = loginUser(USER_NAME_2, USER_PASSWORD_2);
+        String userToken3 = loginUser(USER_NAME_3, USER_PASSWORD_3);
+
+        int gameId = createNewGame(userToken1, false).path("gameId");
+        int nodeIdToBuild = viewGame(userToken1, gameId).path("map.hexes[0].nodes.topLeftId");
+        int edgeIdToBuildFirst = viewGame(userToken1, gameId).path("map.hexes[0].edges.topLeftId");
+        int edgeIdToBuildSecond = viewGame(userToken1, gameId).path("map.hexes[0].edges.topRightId");
+        int gameUserId1 = viewGame(userToken1, gameId).path("gameUsers[0].id");
+
+        joinPublicGame(userToken2, gameId);
+        joinPublicGame(userToken3, gameId);
+
+        setUserReady(userToken1, gameId);
+        setUserReady(userToken2, gameId);
+        setUserReady(userToken3, gameId);
+
+        buildSettlement(userToken1, gameId, nodeIdToBuild);
+
+        viewGame(userToken1, gameId)
+                .then()
+                .statusCode(200)
+                .body("map.hexes[0].edges.topLeftId", is(edgeIdToBuildFirst))
+                .body("map.edges.find {it.edgeId = " + edgeIdToBuildFirst + "}.building", nullValue())
+                .body("map.hexes[0].edges.topRightId", is(edgeIdToBuildSecond))
+                .body("map.edges.find {it.edgeId = " + edgeIdToBuildSecond + "}.building", nullValue())
+                .body("status", equalTo("PLAYING"));
+
+        buildRoad(userToken1, gameId, edgeIdToBuildFirst);
+
+        viewGame(userToken1, gameId)
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("PLAYING"))
+                .body("map.hexes[0].edges.topLeftId", is(edgeIdToBuildFirst))
+                .body("map.hexes[0].edges.topRightId", is(edgeIdToBuildSecond))
+                .body("map.edges.find {it.edgeId = " + edgeIdToBuildSecond + "}.building", nullValue())
+                .rootPath("map.edges.find {it.edgeId == " + edgeIdToBuildFirst + "}")
+                .body("building", notNullValue())
+                .body("building.ownerGameUserId", is(gameUserId1))
+                .body("building.built", equalTo("ROAD"));
+
+        buildRoad(userToken1, gameId, edgeIdToBuildSecond);
+
+        viewGame(userToken1, gameId)
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("PLAYING"))
+                .body("map.hexes[0].edges.topLeftId", is(edgeIdToBuildFirst))
+                .body("map.hexes[0].edges.topRightId", is(edgeIdToBuildSecond))
+                .rootPath("map.edges.find {it.edgeId == " + edgeIdToBuildFirst + "}")
+                .body("building", notNullValue())
+                .body("building.ownerGameUserId", is(gameUserId1))
+                .body("building.built", equalTo("ROAD"))
+                .rootPath("map.edges.find {it.edgeId == " + edgeIdToBuildSecond + "}")
+                .body("building", notNullValue())
+                .body("building.ownerGameUserId", is(gameUserId1))
+                .body("building.built", equalTo("ROAD"));
+
+    }
+
+    @Test
+    public void should_fail_if_try_to_build_road_on_existing_road() {
+        String userToken1 = loginUser(USER_NAME_1, USER_PASSWORD_1);
+        String userToken2 = loginUser(USER_NAME_2, USER_PASSWORD_2);
+        String userToken3 = loginUser(USER_NAME_3, USER_PASSWORD_3);
+
+        int gameId = createNewGame(userToken1, false).path("gameId");
+        int nodeIdToBuild = viewGame(userToken1, gameId).path("map.hexes[0].nodes.topLeftId");
+        int edgeIdToBuild = viewGame(userToken1, gameId).path("map.hexes[0].edges.topLeftId");
+        int gameUserId1 = viewGame(userToken1, gameId).path("gameUsers[0].id");
+
+        joinPublicGame(userToken2, gameId);
+        joinPublicGame(userToken3, gameId);
+
+        setUserReady(userToken1, gameId);
+        setUserReady(userToken2, gameId);
+        setUserReady(userToken3, gameId);
+
+        buildSettlement(userToken1, gameId, nodeIdToBuild);
+
+        viewGame(userToken1, gameId)
+                .then()
+                .statusCode(200)
+                .body("map.hexes[0].edges.topLeftId", is(edgeIdToBuild))
+                .body("map.edges.find {it.edgeId = " + edgeIdToBuild + "}.building", nullValue())
+                .body("status", equalTo("PLAYING"));
+
+        buildRoad(userToken1, gameId, edgeIdToBuild);
+
+        viewGame(userToken1, gameId)
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("PLAYING"))
+                .body("map.hexes[0].edges.topLeftId", is(edgeIdToBuild))
+                .rootPath("map.edges.find {it.edgeId == " + edgeIdToBuild + "}")
+                .body("building", notNullValue())
+                .body("building.ownerGameUserId", is(gameUserId1))
+                .body("building.built", equalTo("ROAD"));
+
+        buildRoad(userToken2, gameId, edgeIdToBuild)
+                .then()
+                .statusCode(400)
+                .body("errorCode", equalTo("ERROR"));
+
+    }
+
+    @Test
+    public void should_fail_if_player_try_to_build_road_without_any_connection_to_settlement_or_city_or_road() {
+        String userToken1 = loginUser(USER_NAME_1, USER_PASSWORD_1);
+        String userToken2 = loginUser(USER_NAME_2, USER_PASSWORD_2);
+        String userToken3 = loginUser(USER_NAME_3, USER_PASSWORD_3);
+
+        int gameId = createNewGame(userToken1, false).path("gameId");
+        int edgeIdToBuild = viewGame(userToken1, gameId).path("map.hexes[0].edges.topLeftId");
 
         joinPublicGame(userToken2, gameId);
         joinPublicGame(userToken3, gameId);
@@ -56,82 +206,98 @@ public class BuildRoadTest extends PlayTestUtil {
         viewGame(userToken1, gameId)
                 .then()
                 .statusCode(200)
-                .body("map.edges[0].edgeId", is(edgeId))
-                .body("map.edges[0].building", nullValue())
+                .body("map.hexes[0].edges.topLeftId", is(edgeIdToBuild))
+                .body("map.edges.find {it.edgeId = " + edgeIdToBuild + "}.building", nullValue())
                 .body("status", equalTo("PLAYING"));
 
-        //TODO: need to build settlement first. add call of this method after its implementation
-        /*
-        buildRoad(userToken1, gameId, edgeId)
+        buildRoad(userToken1, gameId, edgeIdToBuild)
+                .then()
+                .statusCode(400)
+                .body("errorCode", equalTo("ERROR"));
+
+    }
+
+    @Test
+    public void should_fail_if_player_try_to_build_road_without_any_connection_to_his_settlement_or_city_or_road_but_connected_with_opponents_settlement() {
+        String userToken1 = loginUser(USER_NAME_1, USER_PASSWORD_1);
+        String userToken2 = loginUser(USER_NAME_2, USER_PASSWORD_2);
+        String userToken3 = loginUser(USER_NAME_3, USER_PASSWORD_3);
+
+        int gameId = createNewGame(userToken1, false).path("gameId");
+        int nodeIdToBuild = viewGame(userToken1, gameId).path("map.hexes[0].nodes.topLeftId");
+        int edgeIdToBuild = viewGame(userToken1, gameId).path("map.hexes[0].edges.topLeftId");
+
+        joinPublicGame(userToken2, gameId);
+        joinPublicGame(userToken3, gameId);
+
+        setUserReady(userToken1, gameId);
+        setUserReady(userToken2, gameId);
+        setUserReady(userToken3, gameId);
+
+        buildSettlement(userToken2, gameId, nodeIdToBuild);
+
+        viewGame(userToken1, gameId)
+                .then()
+                .statusCode(200)
+                .body("map.hexes[0].edges.topLeftId", is(edgeIdToBuild))
+                .body("map.edges.find {it.edgeId = " + edgeIdToBuild + "}.building", nullValue())
+                .body("status", equalTo("PLAYING"));
+
+        buildRoad(userToken1, gameId, edgeIdToBuild)
+                .then()
+                .statusCode(400)
+                .body("errorCode", equalTo("ERROR"));
+
+    }
+
+    @Test
+    public void should_fail_if_player_try_to_build_road_without_any_connection_to_his_settlement_or_city_or_road_but_connected_with_opponents_road() {
+        String userToken1 = loginUser(USER_NAME_1, USER_PASSWORD_1);
+        String userToken2 = loginUser(USER_NAME_2, USER_PASSWORD_2);
+        String userToken3 = loginUser(USER_NAME_3, USER_PASSWORD_3);
+
+        int gameId = createNewGame(userToken1, false).path("gameId");
+        int nodeIdToBuild = viewGame(userToken1, gameId).path("map.hexes[0].nodes.topLeftId");
+        int edgeIdToBuildFirst = viewGame(userToken1, gameId).path("map.hexes[0].edges.topLeftId");
+        int edgeIdToBuildSecond = viewGame(userToken1, gameId).path("map.hexes[0].edges.topRightId");;
+        int gameUserId1 = viewGame(userToken1, gameId).path("gameUsers[0].id");
+
+        joinPublicGame(userToken2, gameId);
+        joinPublicGame(userToken3, gameId);
+
+        setUserReady(userToken1, gameId);
+        setUserReady(userToken2, gameId);
+        setUserReady(userToken3, gameId);
+
+        buildSettlement(userToken1, gameId, nodeIdToBuild);
+
+        viewGame(userToken1, gameId)
+                .then()
+                .statusCode(200)
+                .body("map.hexes[0].edges.topLeftId", is(edgeIdToBuildFirst))
+                .body("map.edges.find {it.edgeId = " + edgeIdToBuildFirst + "}.building", nullValue())
+                .body("map.hexes[0].edges.topRightId", is(edgeIdToBuildSecond))
+                .body("map.edges.find {it.edgeId = " + edgeIdToBuildSecond + "}.building", nullValue())
+                .body("status", equalTo("PLAYING"));
+
+        buildRoad(userToken1, gameId, edgeIdToBuildFirst);
+
+        viewGame(userToken1, gameId)
                 .then()
                 .statusCode(200)
                 .body("status", equalTo("PLAYING"))
-                .rootPath("map.edges[0]")
-                .body("edgeId", is(edgeId))
-                .body("building.buildingOwner.user.id", is(firstUserId))
+                .body("map.hexes[0].edges.topLeftId", is(edgeIdToBuildFirst))
+                .body("map.hexes[0].edges.topRightId", is(edgeIdToBuildSecond))
+                .body("map.edges.find {it.edgeId = " + edgeIdToBuildSecond + "}.building", nullValue())
+                .rootPath("map.edges.find {it.edgeId == " + edgeIdToBuildFirst + "}")
+                .body("building", notNullValue())
+                .body("building.ownerGameUserId", is(gameUserId1))
                 .body("building.built", equalTo("ROAD"));
-                //TODO: check if there is a settlement on node that belong to edge or if there is another road with mutual node
-        */
-    }
 
-    @Test
-    public void should_successfully_build_road_on_empty_edge_if_has_neighbour_road_that_belongs_to_this_player() {
-        //TODO:IMPLEMENT
-    }
-
-    @Test
-    public void should_fail_if_try_to_build_road_on_existing_road() {
-        String userToken1 = loginUser(USER_NAME_1, USER_PASSWORD_1);
-        String userToken2 = loginUser(USER_NAME_2, USER_PASSWORD_2);
-        String userToken3 = loginUser(USER_NAME_3, USER_PASSWORD_3);
-
-        int gameId = createNewGame(userToken1, false).path("gameId");
-        int edgeId = viewGame(userToken1, gameId).path("map.edges[0].edgeId");
-
-        joinPublicGame(userToken2, gameId);
-        joinPublicGame(userToken3, gameId);
-
-        setUserReady(userToken1, gameId);
-        setUserReady(userToken2, gameId);
-        setUserReady(userToken3, gameId);
-
-        //TODO: need to build settlement first. add call of this method after its implementation
-        /*
-        buildRoad(userToken1, gameId, edgeId);
-
-        buildRoad(userToken2, gameId, edgeId)
+        buildRoad(userToken2, gameId, edgeIdToBuildSecond)
                 .then()
                 .statusCode(400)
                 .body("errorCode", equalTo("ERROR"));
-        */
-    }
-
-    @Test
-    public void should_fail_if_player_try_to_build_road_without_any_connection_to_his_settlement_or_city_or_road() {
-        String userToken1 = loginUser(USER_NAME_1, USER_PASSWORD_1);
-        String userToken2 = loginUser(USER_NAME_2, USER_PASSWORD_2);
-        String userToken3 = loginUser(USER_NAME_3, USER_PASSWORD_3);
-
-        int gameId = createNewGame(userToken1, false).path("gameId");
-        int edgeId = viewGame(userToken1, gameId).path("map.edges[0].edgeId");
-        int firstUserId = getUserId(userToken1);
-
-        joinPublicGame(userToken2, gameId);
-        joinPublicGame(userToken3, gameId);
-
-        setUserReady(userToken1, gameId);
-        setUserReady(userToken2, gameId);
-        setUserReady(userToken3, gameId);
-
-        //build road without any connections
-        buildRoad(userToken1, gameId, edgeId)
-                .then()
-                .statusCode(400)
-                .body("errorCode", equalTo("ERROR"));
-
-        //TODO: build road with connection to smb else's settlement or city
-
-        //TODO: build road with connection to smb else's road
 
     }
 
@@ -142,7 +308,6 @@ public class BuildRoadTest extends PlayTestUtil {
         String userToken3 = loginUser(USER_NAME_3, USER_PASSWORD_3);
 
         int gameId = createNewGame(userToken1, false).path("gameId");
-        int edgeId = viewGame(userToken1, gameId).path("map.edges[0].edgeId");
 
         joinPublicGame(userToken2, gameId);
         joinPublicGame(userToken3, gameId);
@@ -151,18 +316,63 @@ public class BuildRoadTest extends PlayTestUtil {
         setUserReady(userToken2, gameId);
         setUserReady(userToken3, gameId);
 
-        //TODO: need to build settlement first. add call of this method after its implementation
-        /*
-        buildRoad(userToken1, gameId, 999999999)
+        buildRoad(userToken1, gameId, -1)
                 .then()
                 .statusCode(400)
                 .body("errorCode", equalTo("ERROR"));
-         */
+
     }
 
 
     @Test
     public void should_fail_if_edge_has_connection_to_neighbour_road_but_opposite_node_has_building_that_belongs_to_another_player() {
-        //TODO: IMPLEMENT
+        String userToken1 = loginUser(USER_NAME_1, USER_PASSWORD_1);
+        String userToken2 = loginUser(USER_NAME_2, USER_PASSWORD_2);
+        String userToken3 = loginUser(USER_NAME_3, USER_PASSWORD_3);
+
+        int gameId = createNewGame(userToken1, false).path("gameId");
+        int nodeIdToBuildFirst = viewGame(userToken1, gameId).path("map.hexes[0].nodes.topLeftId");
+        int nodeIdToBuildSecond = viewGame(userToken1, gameId).path("map.hexes[0].nodes.topRightId");
+        int edgeIdToBuildFirst = viewGame(userToken1, gameId).path("map.hexes[0].edges.topLeftId");
+        int edgeIdToBuildSecond = viewGame(userToken1, gameId).path("map.hexes[0].edges.topRightId");
+        int gameUserId1 = viewGame(userToken1, gameId).path("gameUsers[0].id");
+
+        joinPublicGame(userToken2, gameId);
+        joinPublicGame(userToken3, gameId);
+
+        setUserReady(userToken1, gameId);
+        setUserReady(userToken2, gameId);
+        setUserReady(userToken3, gameId);
+
+        buildSettlement(userToken1, gameId, nodeIdToBuildFirst);
+        buildSettlement(userToken2, gameId, nodeIdToBuildSecond);
+
+        viewGame(userToken1, gameId)
+                .then()
+                .statusCode(200)
+                .body("map.hexes[0].edges.topLeftId", is(edgeIdToBuildFirst))
+                .body("map.edges.find {it.edgeId = " + edgeIdToBuildFirst + "}.building", nullValue())
+                .body("map.hexes[0].edges.topRightId", is(edgeIdToBuildSecond))
+                .body("map.edges.find {it.edgeId = " + edgeIdToBuildSecond + "}.building", nullValue())
+                .body("status", equalTo("PLAYING"));
+
+        buildRoad(userToken1, gameId, edgeIdToBuildFirst);
+
+        viewGame(userToken1, gameId)
+                .then()
+                .statusCode(200)
+                .body("status", equalTo("PLAYING"))
+                .body("map.hexes[0].edges.topLeftId", is(edgeIdToBuildFirst))
+                .body("map.hexes[0].edges.topRightId", is(edgeIdToBuildSecond))
+                .body("map.edges.find {it.edgeId = " + edgeIdToBuildSecond + "}.building", nullValue())
+                .rootPath("map.edges.find {it.edgeId == " + edgeIdToBuildFirst + "}")
+                .body("building", notNullValue())
+                .body("building.ownerGameUserId", is(gameUserId1))
+                .body("building.built", equalTo("ROAD"));
+
+        buildRoad(userToken1, gameId, edgeIdToBuildSecond)
+                .then()
+                .statusCode(400)
+                .body("errorCode", equalTo("ERROR"));
     }
 }
