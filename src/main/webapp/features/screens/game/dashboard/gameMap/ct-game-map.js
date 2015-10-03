c4032755a95616ccbee778b62884e853d3efe643
Skisor <syrovets@gmail.com>
2015-10-03 17:49:47 +0300

    [US-50] Fix temp buildings

--- src/main/webapp/features/screens/game/dashboard/gameMap/ct-game-map.js
+++ src/main/webapp/features/screens/game/dashboard/gameMap/ct-game-map.js
@@ -13,10 +13,13 @@ angular.module('catan')
 
                     // temp buildings
                     console.log(scope.game.map);
-                    scope.game.map.nodes[3].building = {built: "SETTLEMENT", ownerGameUserId: 28};
-                    scope.game.map.nodes[5].building = {built: "CITY", ownerGameUserId: 29};
-                    scope.game.map.edges[3].building = {built: "ROAD", ownerGameUserId: 28};
-                    scope.game.map.edges[5].building = {built: "ROAD", ownerGameUserId: 30};
+                    var gameUserId1 = scope.game.gameUsers[0].id;
+                    var gameUserId2 = scope.game.gameUsers[1].id;
+                    var gameUserId3 = scope.game.gameUsers[2].id;
+                    scope.game.map.nodes[3].building = {built: "SETTLEMENT", ownerGameUserId: gameUserId1};
+                    scope.game.map.nodes[5].building = {built: "CITY", ownerGameUserId: gameUserId2};
+                    scope.game.map.edges[3].building = {built: "ROAD", ownerGameUserId: gameUserId1};
+                    scope.game.map.edges[5].building = {built: "ROAD", ownerGameUserId: gameUserId3};
 
                     var canvas = angular.element('<div/>').addClass('canvas').appendTo(element);
                     MapDrawService.drawMap(canvas, scope.game, scope.game.map);
