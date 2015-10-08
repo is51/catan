c1f637beb344bf4c7c140b990149fcfba283d4f2
ypetrenko <yuriy.a.petrenko@gmail.com>
2015-10-09 02:34:55 +0300

    [US-51, REFACTOR] create some validation methods, endTurn refactor

--- src/main/java/catan/services/impl/PlayServiceImpl.java
+++ src/main/java/catan/services/impl/PlayServiceImpl.java
@@ -10,7 +10,6 @@ import catan.domain.model.dashboard.types.EdgeBuiltType;
 import catan.domain.model.dashboard.types.NodeBuiltType;
 import catan.domain.model.game.GameBean;
 import catan.domain.model.game.GameUserBean;
-import catan.domain.model.game.types.GameStage;
 import catan.domain.model.game.types.GameStatus;
 import catan.domain.model.user.UserBean;
 import catan.services.PlayService;
@@ -39,11 +38,8 @@ public class PlayServiceImpl implements PlayService {
     public void buildRoad(UserBean user, String gameIdString, String edgeIdString) throws PlayException, GameException {
         log.debug("User {} tries to build road at edge {} of game id {}",
                 user == null ? "<EMPTY>" : user.getUsername(), edgeIdString, gameIdString);
-        //TODO: move to common validation method
-        if (user == null) {
-            log.debug("User should not be empty");
-            throw new PlayException(ERROR_CODE_ERROR);
-        }
+
+        validateUserNotEmpty(user);
 
         int edgeId;
         try {
@@ -59,24 +55,9 @@ public class PlayServiceImpl implements PlayService {
         }
 
         GameBean game = gameUtil.getGameById(gameIdString, ERROR_CODE_ERROR);
-        if (game.getStatus() != GameStatus.PLAYING) {
-            log.debug("Cannot build road in not playing game");
-            throw new GameException(ERROR_CODE_ERROR);
-        }
+        validateGameStatus(game, GameStatus.PLAYING);
 
-        //TODO: move to util method and refactor all other places
-        GameUserBean gameUserBean = null;
-        for (GameUserBean gameUser : game.getGameUsers()) {
-            if (gameUser.getUser().equals(user)) {
-                gameUserBean = gameUser;
-                break;
-            }
-        }
-
-        if (gameUserBean == null) {
-            log.debug("User is not joined to game id specified %s", gameIdString);
-            throw new PlayException(ERROR_CODE_ERROR);
-        }
+        GameUserBean gameUserBean = validateUserIsJoined(user, game);
 
         EdgeBean edgeToBuildOn = null;
         for (EdgeBean edge : game.getEdges()) {
@@ -145,10 +126,7 @@ public class PlayServiceImpl implements PlayService {
     public void buildSettlement(UserBean user, String gameIdString, String nodeIdString) throws PlayException, GameException {
         log.debug("User {} tries to build settlement at node {} of game id {}",
                 user == null ? "<EMPTY>" : user.getUsername(), nodeIdString, gameIdString);
-        if (user == null) {
-            log.debug("User should not be empty");
-            throw new PlayException(ERROR_CODE_ERROR);
-        }
+        validateUserNotEmpty(user);
 
         int nodeId;
         try {
@@ -164,23 +142,8 @@ public class PlayServiceImpl implements PlayService {
         }
 
         GameBean game = gameUtil.getGameById(gameIdString, ERROR_CODE_ERROR);
-        if (game.getStatus() != GameStatus.PLAYING) {
-            log.debug("Cannot build settlement in not playing game");
-            throw new GameException(ERROR_CODE_ERROR);
-        }
-
-        GameUserBean gameUserBean = null;
-        for (GameUserBean gameUser : game.getGameUsers()) {
-            if (gameUser.getUser().equals(user)) {
-                gameUserBean = gameUser;
-                break;
-            }
-        }
-
-        if (gameUserBean == null) {
-            log.debug("User is not joined to game {}", gameIdString);
-            throw new PlayException(ERROR_CODE_ERROR);
-        }
+        validateGameStatus(game, GameStatus.PLAYING);
+        GameUserBean gameUserBean = validateUserIsJoined(user, game);
 
         NodeBean nodeToBuildOn = null;
         for (NodeBean node : game.getNodes()) {
@@ -243,36 +206,15 @@ public class PlayServiceImpl implements PlayService {
 
     @Override
     public void endTurn(UserBean user, String gameIdString) throws PlayException, GameException {
-        Integer nextMoveNumber = null;
+        Integer nextMoveNumber;
         GameBean game = gameUtil.getGameById(gameIdString, ERROR_CODE_ERROR);
         List<List<String>> initialBuildingsSet = gameUtil.getInitialBuildingsSetFromJson(game.getInitialBuildingsSet());
 
-        log.debug("User {} tries to end his turn of game id {}",
-                user == null ? "<EMPTY>" : user.getUsername(), gameIdString);
-        //TODO: move to common validation method
-        if (user == null) {
-            log.debug("User should not be empty");
-            throw new PlayException(ERROR_CODE_ERROR);
-        }
+        log.debug("User {} tries to end his turn of game id {}",user == null ? "<EMPTY>" : user.getUsername(), gameIdString);
 
-        if (game.getStatus() != GameStatus.PLAYING) {
-            log.debug("Cannot end turn of not playing game");
-            throw new GameException(ERROR_CODE_ERROR);
-        }
-
-        //TODO: move to util method and refactor all other places
-        GameUserBean gameUserBean = null;
-        for (GameUserBean gameUser : game.getGameUsers()) {
-            if (gameUser.getUser().equals(user)) {
-                gameUserBean = gameUser;
-                break;
-            }
-        }
-
-        if (gameUserBean == null) {
-            log.debug("User is not joined to game with specified id {}", gameIdString);
-            throw new PlayException(ERROR_CODE_ERROR);
-        }
+        validateUserNotEmpty(user);
+        validateGameStatus(game, GameStatus.PLAYING);
+        GameUserBean gameUserBean = validateUserIsJoined(user, game);
 
         if (!game.getCurrentMove().equals(gameUserBean.getMoveOrder())) {
             log.debug("It is not current turn of user {}", user.getUsername());
@@ -281,24 +223,10 @@ public class PlayServiceImpl implements PlayService {
 
         switch (game.getStage()) {
             case PREPARATION:
-                Integer preparationCycle = game.getPreparationCycle();
-                if (isFirstMove(game) && !isOddCycle(preparationCycle) || isLastMove(game) && isOddCycle(preparationCycle)) {
-                    if (preparationCycle == initialBuildingsSet.size()) {
-                        game.setStage(GameStage.MAIN);
-                        game.setPreparationCycle(null);
-                        log.debug("Game Stage was changed from PREPARATION to {}", game.getStage());
-                        nextMoveNumber = 1;
-                    } else {
-                        nextMoveNumber = endTurnUtil.getNextMoveInPreparationStage(game);
-                        game.setPreparationCycle(preparationCycle + 1);
-                        log.debug("Preparation cycle increased by 1. Current preparation cycle is {} of {}", game.getPreparationCycle(), initialBuildingsSet.size());
-                    }
-                } else {
-                    nextMoveNumber = endTurnUtil.getNextMoveInPreparationStage(game);
-                }
+                nextMoveNumber = endTurnUtil.endTurnImplInPreparationStage(game, initialBuildingsSet);
                 break;
             case MAIN:
-                nextMoveNumber = endTurnUtil.getNextMoveInMainStage(game);
+                nextMoveNumber = endTurnUtil.endTurnImplInMainStage(game);
                 break;
             default:
                 log.debug("Cannot recognize current game stage: {}", game.getStage());
@@ -308,21 +236,38 @@ public class PlayServiceImpl implements PlayService {
         log.debug("Next move order in {} stage is changing from {} to {}", game.getStage(), game.getCurrentMove(), nextMoveNumber);
         game.setCurrentMove(nextMoveNumber);
 
-        //TODO: in Preparation mode set to 1, in Main mode ignore this field
-        game.setCurrentCycleBuildingNumber(1);
         gameDao.updateGame(game);
     }
 
-    private boolean isOddCycle(Integer preparationCycle) {
-        return preparationCycle % 2 > 0;
+    private void validateUserNotEmpty(UserBean user) throws PlayException {
+        if (user == null) {
+            log.debug("User should not be empty");
+            throw new PlayException(ERROR_CODE_ERROR);
+        }
     }
 
-    private boolean isFirstMove(GameBean game) {
-        return game.getCurrentMove() == 1;
+    private GameUserBean validateUserIsJoined(UserBean user, GameBean game) throws PlayException {
+        GameUserBean gameUserBean = null;
+        for (GameUserBean gameUser : game.getGameUsers()) {
+            if (gameUser.getUser().equals(user)) {
+                gameUserBean = gameUser;
+                break;
+            }
+        }
+
+        if (gameUserBean == null) {
+            log.debug("User is not joined to game {}", game.getGameId());
+            throw new PlayException(ERROR_CODE_ERROR);
+        }
+
+        return gameUserBean;
     }
 
-    private boolean isLastMove(GameBean game) {
-        return game.getCurrentMove() == game.getGameUsers().size();
+    private void validateGameStatus(GameBean game, GameStatus expectedStatus) throws GameException {
+        if (game.getStatus() != expectedStatus) {
+            log.debug("Cannot end turn of not playing game");
+            throw new GameException(ERROR_CODE_ERROR);
+        }
     }
 
     @Autowired
@@ -340,4 +285,3 @@ public class PlayServiceImpl implements PlayService {
         this.endTurnUtil = endTurnUtil;
     }
 }
-
--- src/main/java/catan/services/util/play/EndTurnUtil.java
+++ src/main/java/catan/services/util/play/EndTurnUtil.java
@@ -1,10 +1,43 @@
 package catan.services.util.play;
 
 import catan.domain.model.game.GameBean;
+import catan.domain.model.game.types.GameStage;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 @Component
 public class EndTurnUtil {
+    private Logger log = LoggerFactory.getLogger(EndTurnUtil.class);
+
+    public Integer endTurnImplInPreparationStage(GameBean game, List<List<String>> initialBuildingsSet) {
+        Integer nextMoveNumber;
+        Integer preparationCycle = game.getPreparationCycle();
+        game.setCurrentCycleBuildingNumber(1);
+        if (isFirstMove(game) && !isOddCycle(preparationCycle) || isLastMove(game) && isOddCycle(preparationCycle)) {
+            if (preparationCycle == initialBuildingsSet.size()) {
+                game.setStage(GameStage.MAIN);
+                game.setPreparationCycle(null);
+                log.debug("Game Stage was changed from PREPARATION to {}", game.getStage());
+                nextMoveNumber = 1;
+            } else {
+                nextMoveNumber = getNextMoveInPreparationStage(game);
+                game.setPreparationCycle(preparationCycle + 1);
+                log.debug("Preparation cycle increased by 1. Current preparation cycle is {} of {}", game.getPreparationCycle(), initialBuildingsSet.size());
+            }
+        } else {
+            nextMoveNumber = getNextMoveInPreparationStage(game);
+        }
+        return nextMoveNumber;
+    }
+
+    public int endTurnImplInMainStage(GameBean game) {
+        return game.getCurrentMove().equals(game.getGameUsers().size())
+                ? 1
+                : game.getCurrentMove() + 1;
+    }
 
     public Integer getNextMoveInPreparationStage(GameBean game) {
         Integer currentMove = game.getCurrentMove();
@@ -16,9 +49,15 @@ public class EndTurnUtil {
         }
     }
 
-    public Integer getNextMoveInMainStage(GameBean game) {
-        return game.getCurrentMove().equals(game.getGameUsers().size())
-                ? 1
-                : game.getCurrentMove() + 1;
+    private boolean isOddCycle(Integer preparationCycle) {
+        return preparationCycle % 2 > 0;
+    }
+
+    private boolean isFirstMove(GameBean game) {
+        return game.getCurrentMove() == 1;
+    }
+
+    private boolean isLastMove(GameBean game) {
+        return game.getCurrentMove() == game.getGameUsers().size();
     }
 }
