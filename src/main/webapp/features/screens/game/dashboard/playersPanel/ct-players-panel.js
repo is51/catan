f8a77ef02c9b1796be370d89002c178c06dd68b8
Skisor <syrovets@gmail.com>
2015-10-10 18:41:25 +0300

    [US-50] Fix sorting of users in players' panel

--- src/main/webapp/features/screens/game/dashboard/playersPanel/ct-players-panel.js
+++ src/main/webapp/features/screens/game/dashboard/playersPanel/ct-players-panel.js
@@ -17,9 +17,10 @@ angular.module('catan')
 
                     function usersSortedByMoveOrderCurrentUserFirst(game) {
                         var currentGameUser = game.getCurrentUser();
-                        var usersCount = game.gameUsers.length;
+                        var gameUsers = game.gameUsers.map(function(user) { return user; });
+                        var usersCount = gameUsers.length;
 
-                        return game.gameUsers.sort(function(a, b) {
+                        return gameUsers.sort(function(a, b) {
                             var aMoveOrder = a.moveOrder + ((a !== currentGameUser && a.moveOrder < currentGameUser.moveOrder) ? usersCount : 0);
                             var bMoveOrder = b.moveOrder + ((b !== currentGameUser && b.moveOrder < currentGameUser.moveOrder) ? usersCount : 0);
 
