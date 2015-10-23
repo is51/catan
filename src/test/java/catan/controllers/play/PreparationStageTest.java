b0cac159cb4c6776d1a94f186aeaa6568ee84638
Skisor <syrovets@gmail.com>
2015-10-23 13:04:48 +0300

    [US-52] Functional tests

--- src/test/java/catan/controllers/play/PreparationStageTest.java
+++ src/test/java/catan/controllers/play/PreparationStageTest.java
@@ -246,4 +246,84 @@ public class PreparationStageTest extends PlayTestUtil {
     }
 
 
+    @Test
+    public void should_provide_correct_available_actions_and_build_correctly_during_preparation_stage_buildings_set_2() {
+        String userToken1 = loginUser(USER_NAME_1, USER_PASSWORD_1);
+        String userToken2 = loginUser(USER_NAME_2, USER_PASSWORD_2);
+        String userToken3 = loginUser(USER_NAME_3, USER_PASSWORD_3);
+        String[] userTokens = {userToken1, userToken2, userToken3};
+
+        int gameId = createNewGame(userToken1, false, 12, 2).path("gameId");
+
+        joinPublicGame(userToken2, gameId);
+        joinPublicGame(userToken3, gameId);
+
+        setUserReady(userToken1, gameId);
+        setUserReady(userToken2, gameId);
+        setUserReady(userToken3, gameId);
+
+        int gameUserNumber0MoveOrder = viewGame(userToken1, gameId).path("gameUsers[0].moveOrder");
+        int gameUserNumber1MoveOrder = viewGame(userToken1, gameId).path("gameUsers[1].moveOrder");
+
+        int firstGameUserNumber = (gameUserNumber0MoveOrder == 1) ? 0 : ((gameUserNumber1MoveOrder == 1) ? 1 : 2);
+        int secondGameUserNumber = (gameUserNumber0MoveOrder == 2) ? 0 : ((gameUserNumber1MoveOrder == 2) ? 1 : 2);
+        int thirdGameUserNumber = (gameUserNumber0MoveOrder == 3) ? 0 : ((gameUserNumber1MoveOrder == 3) ? 1 : 2);
+
+        /* Achtung! Nodes and edges are correct only for currently generated map
+        *     --------------
+        *     | 7 |12 | 16 |
+        *   -------------------
+        *   | 3 | 8 | 13 | 17 |
+        * -----------------------
+        * | 0 | 4 | 9 | 14 | 18 |
+        * -----------------------
+        *   | 1 | 5 | 10 | 15 |
+        *   -------------------
+        *     | 2 | 6 | 11 |
+        *     --------------
+        */
+        int nodeId1ToBuildForFirstUser = viewGame(userToken1, gameId).path("map.hexes[0].nodesIds.topLeftId");
+        int edgeId1ToBuildForFirstUser = viewGame(userToken1, gameId).path("map.hexes[0].edgesIds.topLeftId");
+        int nodeId2ToBuildForFirstUser = viewGame(userToken1, gameId).path("map.hexes[0].nodesIds.topRightId");
+        int edgeId2ToBuildForFirstUser = viewGame(userToken1, gameId).path("map.hexes[0].edgesIds.rightId");
+        int nodeId3ToBuildForFirstUser = viewGame(userToken1, gameId).path("map.hexes[0].nodesIds.bottomId");
+        int edgeId3ToBuildForFirstUser = viewGame(userToken1, gameId).path("map.hexes[0].edgesIds.bottomLeftId");
+
+        int nodeId1ToBuildForSecondUser = viewGame(userToken1, gameId).path("map.hexes[8].nodesIds.topLeftId");
+        int edgeId1ToBuildForSecondUser = viewGame(userToken1, gameId).path("map.hexes[8].edgesIds.topLeftId");
+        int nodeId2ToBuildForSecondUser = viewGame(userToken1, gameId).path("map.hexes[8].nodesIds.topRightId");
+        int edgeId2ToBuildForSecondUser = viewGame(userToken1, gameId).path("map.hexes[8].edgesIds.rightId");
+        int nodeId3ToBuildForSecondUser = viewGame(userToken1, gameId).path("map.hexes[0].nodesIds.bottomId");
+        int edgeId3ToBuildForSecondUser = viewGame(userToken1, gameId).path("map.hexes[0].edgesIds.bottomLeftId");
+
+        int nodeId1ToBuildForThirdUser = viewGame(userToken1, gameId).path("map.hexes[10].nodesIds.topLeftId");
+        int edgeId1ToBuildForThirdUser = viewGame(userToken1, gameId).path("map.hexes[10].edgesIds.topLeftId");
+        int nodeId2ToBuildForThirdUser = viewGame(userToken1, gameId).path("map.hexes[10].nodesIds.topRightId");
+        int edgeId2ToBuildForThirdUser = viewGame(userToken1, gameId).path("map.hexes[10].edgesIds.rightId");
+        int nodeId3ToBuildForThirdUser = viewGame(userToken1, gameId).path("map.hexes[0].nodesIds.bottomId");
+        int edgeId3ToBuildForThirdUser = viewGame(userToken1, gameId).path("map.hexes[0].edgesIds.bottomLeftId");
+
+        // First player moves #1
+        checkAvailableForUserActionsDuringMove(userTokens, gameId, firstGameUserNumber, secondGameUserNumber, thirdGameUserNumber, nodeId1ToBuildForFirstUser, "BUILD_SETTLEMENT", edgeId1ToBuildForFirstUser);
+        // Second player moves #1
+        checkAvailableForUserActionsDuringMove(userTokens, gameId, secondGameUserNumber, firstGameUserNumber, thirdGameUserNumber, nodeId1ToBuildForSecondUser, "BUILD_SETTLEMENT", edgeId1ToBuildForSecondUser);
+        // Third player moves #1
+        checkAvailableForUserActionsDuringMove(userTokens, gameId, thirdGameUserNumber, secondGameUserNumber, firstGameUserNumber, nodeId1ToBuildForThirdUser, "BUILD_SETTLEMENT", edgeId1ToBuildForThirdUser);
+
+        // Third player moves #2
+        checkAvailableForUserActionsDuringMove(userTokens, gameId, thirdGameUserNumber, secondGameUserNumber, firstGameUserNumber, nodeId2ToBuildForThirdUser, "BUILD_CITY", edgeId2ToBuildForThirdUser);
+        // Second player moves #2
+        checkAvailableForUserActionsDuringMove(userTokens, gameId, secondGameUserNumber, firstGameUserNumber, thirdGameUserNumber, nodeId2ToBuildForSecondUser, "BUILD_CITY", edgeId2ToBuildForSecondUser);
+        // First player moves #2
+        checkAvailableForUserActionsDuringMove(userTokens, gameId, firstGameUserNumber, secondGameUserNumber, thirdGameUserNumber, nodeId2ToBuildForFirstUser, "BUILD_CITY", edgeId2ToBuildForFirstUser);
+
+        // First player moves #3
+        checkAvailableForUserActionsDuringMove(userTokens, gameId, firstGameUserNumber, secondGameUserNumber, thirdGameUserNumber, nodeId3ToBuildForFirstUser, "BUILD_SETTLEMENT", edgeId3ToBuildForFirstUser);
+        // Second player moves #3
+        checkAvailableForUserActionsDuringMove(userTokens, gameId, secondGameUserNumber, firstGameUserNumber, thirdGameUserNumber, nodeId3ToBuildForSecondUser, "BUILD_SETTLEMENT", edgeId3ToBuildForSecondUser);
+        // Third player moves #3
+        checkAvailableForUserActionsDuringMove(userTokens, gameId, thirdGameUserNumber, secondGameUserNumber, firstGameUserNumber, nodeId3ToBuildForThirdUser, "BUILD_SETTLEMENT", edgeId3ToBuildForThirdUser);
+    }
+
+
 }
