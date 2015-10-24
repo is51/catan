5754a0a3003912442e3122e795e5f6690b3e1114
ypetrenko <yuriy.a.petrenko@gmail.com>
2015-10-25 00:08:11 +0300

    [US-61, T276] add functional tests

--- src/test/java/catan/controllers/play/PlayTestUtil.java
+++ src/test/java/catan/controllers/play/PlayTestUtil.java
@@ -9,6 +9,7 @@ public abstract class PlayTestUtil extends GameTestUtil {
 
     protected static final String URL_BUILD_ROAD = "/api/play/build/road";
     protected static final String URL_BUILD_SETTLEMENT = "/api/play/build/settlement";
+    protected static final String URL_BUILD_CITY = "/api/play/build/city";
     protected static final String URL_END_TURN = "/api/play/end-turn";
     
     protected Response buildSettlement(String token, int gameId, int nodeId) {
@@ -18,6 +19,15 @@ public abstract class PlayTestUtil extends GameTestUtil {
                 .parameters("token", token, "gameId", gameId, "nodeId", nodeId)
                 .when()
                 .post(URL_BUILD_SETTLEMENT);
+    }
+
+    protected Response buildCity(String token, int gameId, int nodeId) {
+        return given()
+                .port(SERVER_PORT)
+                .header("Accept", ACCEPT_CONTENT_TYPE)
+                .parameters("token", token, "gameId", gameId, "nodeId", nodeId)
+                .when()
+                .post(URL_BUILD_CITY);
     }
 
     protected Response buildRoad(String token, int gameId, int edgeId) {
