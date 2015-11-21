b990444d49eb0179fc25f9280cc0a5bf094b8d71
Skisor <syrovets@gmail.com>
2015-11-21 14:12:06 +0800

    [US-67] Functional tests: Add "get" - path for GameUserValidator; Add resources checking in GameUserValidator

--- src/test/java/catan/controllers/ctf/GameUserValidator.java
+++ src/test/java/catan/controllers/ctf/GameUserValidator.java
@@ -2,6 +2,7 @@ package catan.controllers.ctf;
 
 import org.hamcrest.Matcher;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 
@@ -18,6 +19,11 @@ public class GameUserValidator {
     public Scenario check(String gameUserAttribute, Matcher matcher) {
         scenario.currentGameDetails.body("gameUsers.find { it.moveOrder == " + moveOrder  + "}." + gameUserAttribute, matcher);
         return scenario;
+    }
+
+    // TODO: ability to use any type, not only int   ( <T> ? )
+    public int get(String gameUserAttribute) {
+        return scenario.currentGameDetails.extract().path("gameUsers.find { it.moveOrder == " + moveOrder + "}." + gameUserAttribute);
     }
 
     // Can be used later
@@ -37,6 +43,16 @@ public class GameUserValidator {
 
     public Scenario doesntHaveAvailableAction(String action) {
         check("availableActions.list.find {it.code == '" + action + "'}", nullValue());
+        return scenario;
+    }
+
+    public Scenario resources(int brick, int wood, int sheep, int wheat, int stone) {
+        check("resources", notNullValue());
+        check("resources.brick", equalTo(brick));
+        check("resources.wood", equalTo(wood));
+        check("resources.sheep", equalTo(sheep));
+        check("resources.wheat", equalTo(wheat));
+        check("resources.stone", equalTo(stone));
         return scenario;
     }
 
