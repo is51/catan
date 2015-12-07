1a735b3acda5140bc5136ace646e5542871aa8b1
Skisor <syrovets@gmail.com>
2015-12-07 11:59:34 +0800

    [US-73] Front-end: Fix "Build 1 roads" -> "Build 1 road"

--- src/main/webapp/features/screens/game/dashboard/cardsPanel/ct-cards-panel.js
+++ src/main/webapp/features/screens/game/dashboard/cardsPanel/ct-cards-panel.js
@@ -29,7 +29,8 @@ angular.module('catan')
                     scope.useCardRoadBuilding = function() {
                         ModalWindowService.hide("CARDS_PANEL");
                         PlayService.useCardRoadBuilding(scope.game).then(function(response) {
-                            alert("Build " + response.data.roadsCount + " roads");
+                            var count = response.data.roadsCount;
+                            alert("Build " + count + " road" + ((count===1)?"":"s"));
                             GameService.refresh(scope.game);
                         }, function(reason) {
                             alert('Error: ' + ((reason.data.errorCode) ? reason.data.errorCode : 'unknown'));
