b4c89f0efd177c1a536ae81b28344fcbfa15d644
ypetrenko <yuriy.a.petrenko@gmail.com>
2016-02-17 18:49:23 +0200

    [US-59, T495] Calculate available places for city building

--- src/main/java/catan/services/util/play/ActionParamsUtil.java
+++ src/main/java/catan/services/util/play/ActionParamsUtil.java
@@ -2,8 +2,10 @@ package catan.services.util.play;
 
 import catan.domain.exception.GameException;
 import catan.domain.model.dashboard.NodeBean;
+import catan.domain.model.dashboard.types.NodeBuiltType;
 import catan.domain.model.game.GameBean;
 import catan.domain.model.game.GameUserBean;
+import catan.domain.model.game.types.GameStage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -18,27 +20,45 @@ public class ActionParamsUtil {
     private Logger log = LoggerFactory.getLogger(ActionParamsUtil.class);
 
     public List<Integer> calculateBuildSettlementParams(GameBean game, GameUserBean gameUser) throws GameException {
+
+        if (game.getStage().equals(GameStage.PREPARATION)) {
+            return fetchNodeIdsToBuildOnInPreparationStage(game);
+        }
+
         List<Integer> nodeIdsToBuildOn = new ArrayList<Integer>();
-        switch (game.getStage()) {
-            case PREPARATION:
-                for (NodeBean node : game.getNodes()) {
-                    if (node.getBuilding() == null && node.noBuildingsOnNeighbourNodes()) {
-                        nodeIdsToBuildOn.add(node.getId());
-                    }
-                }
-                break;
-            case MAIN:
-                for (NodeBean node : game.getNodes()) {
-                    if (node.getBuilding() == null
-                            && node.noBuildingsOnNeighbourNodes()
-                            && node.nearGameUsersNeighbourRoad(gameUser)) {
-                        nodeIdsToBuildOn.add(node.getId());
-                    }
-                }
-                break;
-            default:
-                log.debug("Cannot recognize current game stage: {}", game.getStage());
-                throw new GameException(ERROR_CODE_ERROR);
+        for (NodeBean node : game.getNodes()) {
+            if (node.getBuilding() == null
+                    && node.noBuildingsOnNeighbourNodes()
+                    && node.nearGameUsersNeighbourRoad(gameUser)) {
+                nodeIdsToBuildOn.add(node.getId());
+            }
+        }
+        return nodeIdsToBuildOn;
+    }
+
+    public List<Integer> calculateBuildCityParams(GameBean game, GameUserBean gameUser) throws GameException {
+
+        if (game.getStage().equals(GameStage.PREPARATION)) {
+            return fetchNodeIdsToBuildOnInPreparationStage(game);
+        }
+
+        List<Integer> nodeIdsToBuildOn = new ArrayList<Integer>();
+        for (NodeBean node : game.getNodes()) {
+            if (node.getBuilding() != null
+                    && node.getBuilding().getBuildingOwner().equals(gameUser)
+                    && node.getBuilding().getBuilt().equals(NodeBuiltType.SETTLEMENT)) {
+                nodeIdsToBuildOn.add(node.getId());
+            }
+        }
+        return nodeIdsToBuildOn;
+    }
+
+    private List<Integer> fetchNodeIdsToBuildOnInPreparationStage(GameBean game) {
+        List<Integer> nodeIdsToBuildOn = new ArrayList<Integer>();
+        for (NodeBean node : game.getNodes()) {
+            if (node.getBuilding() == null && node.noBuildingsOnNeighbourNodes()) {
+                nodeIdsToBuildOn.add(node.getId());
+            }
         }
         return nodeIdsToBuildOn;
     }
